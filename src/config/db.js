9ef7c5fc0a18bdff346e2b1f08b01671d2285acc
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-01 11:26:14 -0300

    Tablas añadidas para su creación automática

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,24 @@
 import { sequelize } from "./database.js";
+import { UserModel } from "../models/user.model.js";
+import { ProfileModel } from "../models/profile.model.js";
+import { ArticleModel } from "../models/article.model.js";
+import { TagModel } from "../models/tag.model.js";
+import { ArticleTagModel } from "../models/article_tag.model.js";
 
 export const startDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Se autenticó exitosamente la base de datos");
-    await sequelize.sync();
+    await sequelize.sync({ alter: true });
+
     console.log("Se sincronizó exitosamente con la base de datos");
   } catch (err) {
     console.error("No se pudo conectar con la base de datos", err);
   }
+  await UserModel.sync(),
+    ProfileModel.sync(),
+    ArticleModel.sync(),
+    TagModel.sync(),
+    ArticleTagModel.sync();
+  console.log("Tablas creadas");
 };
