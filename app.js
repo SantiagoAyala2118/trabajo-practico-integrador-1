d70a60c020d3a2b3408c9ea1c6701a64c4f46b9a
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-01 11:35:50 -0300

    startDB añadido para la conexión con la base de datos

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { startDB } from "./src/config/db.js";
+startDB();
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-// app.use();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
