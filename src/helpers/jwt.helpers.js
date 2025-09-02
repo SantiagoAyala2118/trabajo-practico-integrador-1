a489ca47fd499c70cf925e46df18193b6cb276ee
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-02 15:37:43 -0300

    Función para generar un token completada

--- src/helpers/jwt.helpers.js
+++ src/helpers/jwt.helpers.js
@@ -1,3 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+//FUNCIÓN PARA GENERAR UN TOKEN
 export const generateToken = async (user) => {
-  const token = jwt.sign({});
+  const token = jwt.sign(
+    {
+      id: user.id,
+      first_name: user.profile.first_name,
+      lastname: user.profile.lastname,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  return token;
+};
+
+//FUNCION PARA VERIFICAR UN TOKEN
+export const verifyToken = (token) => {
+  try {
+  } catch (err) {
+    console.error("Server error while verifying the token");
+    return resizeBy.status(500).json({
+      message: "Server error while verifying the token",
+    });
+  }
 };
