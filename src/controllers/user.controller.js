e3281b60877364dbc5917fddde0fc59a2e356f07
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-03 09:29:56 -0300

    CRUD completo agregado

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,93 @@
+// Users (acceso admin):
+// ● GET /api/users → Listar todos los usuarios con sus perfiles. (solo admin)
+// ● GET /api/users/:id → Obtener usuario específico con perfil y artículos. (solo admin)
+// ● PUT /api/users/:id → Actualizar usuario (solo admin).
+// ● DELETE /api/users/:id → Eliminación lógica de usuario (solo admin).
+
+import { matchedData } from "express-validator";
+import { UserModel } from "../models/user.model.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const user = await UserModel.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+      include: {
+        model: ProfileModel,
+        as: "profile",
+      },
+    });
+
+    return res.status(200).json({
+      user: user,
+    });
+  } catch (error) {
+    console.error("Server error while getting all users", err);
+    return res.status(500).json({
+      message: "Server error while getting all users",
+    });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await UserModel.findOne(
+      { where: { id: req.params.id } },
+      {
+        attributes: {
+          exclude: ["password"],
+        },
+        include: {
+          model: ProfileModel,
+          as: "profile",
+        },
+      }
+    );
+
+    return res.status(200).json({
+      user: user,
+    });
+  } catch (error) {
+    console.error("Server error while getting user", err);
+    return res.status(500).json({
+      message: "Server error while getting user",
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const validatedData = matchedData(req, { locations: ["body"] });
+
+    if (Object.keys(validatedData) === 0) {
+      return res.status(400).json({
+        message: "You did not send anything to update",
+      });
+    }
+
+    await UserModel.update(validatedData, {
+      where: { id: req.params.id },
+    });
+
+    return res.status(200).json({
+      message: "User updated",
+    });
+  } catch (err) {
+    console.error("Server error while updating user", err);
+    return res.status(500).json({
+      message: "Server error while updating user",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await UserModel.destroy({ where: { id: req.params.id } });
+  } catch (err) {
+    console.error("Server error while deleting user", err);
+    return res.status(500).json({
+      message: "Server error while deleting user",
+    });
+  }
+};
