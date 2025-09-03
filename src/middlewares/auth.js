6796f0b34487198073e8aafd80f109a482324792
Santiago Ayala <santitomayala07@gmail.com>
2025-09-02 22:15:51 -0300

    Función para verificar el token completada (sujeta a cambios)

---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+import { verifyToken } from "../helpers/jwt.helpers.js";
+//MIDDLEWARE PARA VERIFICAR QUE EL USUARIO QUE QUIERE HACER CUALQUIER COSA, ESTÁ AUTENTICADO
+export const authMiddleware = async (req, res, next) => {
+  try {
+    //ONTENGO UN TOKEN DE LA REQUEST
+    const token = req.cookies["token"];
+
+    //SI NO ESTÁ AUTENTICADO, TIRO UN ERROR
+    if (!token) {
+      return res.status(401).json({
+        message: "User not autenticated",
+      });
+    }
+
+    //DECODIFICO EL TOKEN
+    const decoded = verifyToken(token);
+
+    //ALMACENO LA INFO DEL TOKEN DECODIFICADO
+    req.userLogged = decoded;
+
+    next();
+  } catch (err) {
+    console.error("Server error while checking the authentication", err);
+    return res.status(500).json({
+      message: "Server error while checking the authentication",
+    });
+  }
+};
