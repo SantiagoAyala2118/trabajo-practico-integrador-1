1f634a400cc744b81c531201ff915b1c1a1da4fd
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-03 13:17:16 -0300

    Validaciones de usuario, articulo y tag casi terminados

---
+++ src/middlewares/validations/article.validations.js
@@ -0,0 +1,60 @@
+import { body } from "express-validator";
+import { UserModel } from "../../models/user.model.js";
+
+//VALIDACIONES PARA CREAR UN ARTÍCULO
+export const crateArticleValidations = [
+  body("title")
+    .trim()
+    .notEmpty()
+    .withMessage("Title cannot be empty")
+    .isLength({ min: 3, max: 200 })
+    .withMessage(
+      "Title must contain at least 3 characters and a maximum of 200"
+    ),
+  body("content")
+    .trim()
+    .notEmpty()
+    .withMessage("Content cannot be empty")
+    .isLength({ min: 50 })
+    .withMessage("Content must contain at least 50 characters"),
+  body("excerpt")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Excerpt cannot be empty")
+    .isLength({ max: 500 })
+    .withMessage("Excerpt cannot exceed 500 characters"),
+  body("status")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Status cannot be empty")
+    .isIn(["published", "archived"])
+    .withMessage("Status must be either 'published' or 'archived0'"),
+  body("user_id")
+    .trim()
+    .notEmpty()
+    .withMessage("User_id cannot be empty")
+    .isInt({ gt: 0 })
+    .withMessage("User_id must be a number greater than zero")
+    .custom(async (user_id, { req }) => {
+      try {
+        const userExisting = await UserModel.findOne({
+          where: { id: req.body.user_id },
+        });
+
+        if (!userExisting) {
+          return Promise.reject("There is no user in the DB with that ID");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the user by id");
+        return Promise.reject("Error checking the existency of the user by id");
+      }
+    }),
+];
+// Article:
+// ● title: 3-200 caracteres, obligatorio.
+// ● content: mínimo 50 caracteres, obligatorio.
+// ● excerpt: máximo 500 caracteres.
+// ● status: solo valores permitidos ('published', 'archived').
+// ● user_id: debe existir y coincidir con usuario autenticado (excepto admin).
---
+++ src/middlewares/validations/tag.validations.js
@@ -0,0 +1,29 @@
+import { body } from "express-validator";
+import { TagModel } from "../models/tag.model.js";
+
+export const createTagValidations = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name cannot be empty")
+    .isLength({ min: 2, max: 30 })
+    .withMessage("Name must contain at leas 2 characters and a maximum of 30")
+    .matches(/^(?=.*[A-Za-z])(?!.*\s)[A-Za-z]+$/)
+    .custom(async (name, { req }) => {
+      try {
+        const nameExisting = await TagModel.findOne({
+          where: { name: req.body.name },
+        });
+
+        if (nameExisting) {
+          return Promise.reject("There is already a tag with that name");
+        }
+      } catch (err) {
+        console.error("Error checking the viability of the name", err);
+        return Promise.reject("Error checking the viability of the name");
+      }
+    }),
+];
+
+// Tag:
+// ● name: 2-30 caracteres, único, obligatorio, sin espacios.
