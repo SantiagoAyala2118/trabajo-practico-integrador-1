2304511ef3ed94bf2c39d28b6a26c5f406d5b82d
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-04 14:11:14 -0300

    Se quitó la creación de las tablas individualmente

--- src/config/db.js
+++ src/config/db.js
@@ -9,16 +9,12 @@ export const startDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Se autenticó exitosamente la base de datos");
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
 
-    console.log("Se sincronizó exitosamente con la base de datos");
+    console.log(
+      "Se sincronizó exitosamente con la base de datos. Tablas creadas"
+    );
   } catch (err) {
     console.error("No se pudo conectar con la base de datos", err);
   }
-  await UserModel.sync(),
-    ProfileModel.sync(),
-    ArticleModel.sync(),
-    TagModel.sync(),
-    ArticleTagModel.sync();
-  console.log("Tablas creadas");
 };
