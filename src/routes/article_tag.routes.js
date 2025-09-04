1b990153ef0efab2407bc3ca801c7b858a52e93f
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-04 15:51:57 -0300

    Rutas y middlewares añadidos correctamente

--- src/routes/article_tag.routes.js
+++ src/routes/article_tag.routes.js
@@ -1,1 +1,33 @@
 import { Router } from "express";
+import { authAuthorMiddleware } from "../middlewares/ownerMiddleware.js";
+import {
+  createArticleTagValidations,
+  delteArticleTagValidations,
+} from "../middlewares/validations/article_tag.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+import {
+  createArticleTag,
+  deleteArticleTag,
+} from "../controllers/article_tag.controller.js";
+
+const articleTagsRouter = Router();
+
+articleTagsRouter.post(
+  "/api/articles-tags",
+  authAuthorMiddleware,
+  createArticleTagValidations,
+  applyValidations,
+  createArticleTag
+);
+
+articleTagsRouter.delete(
+  "/api/articles-tags/:articleTagId",
+  authAuthorMiddleware,
+  delteArticleTagValidations,
+  applyValidations,
+  deleteArticleTag
+);
+
+// Articles Tags:
+// ● POST /api/articles-tags → Agregar etiqueta a artículo. (solo autor)
+// ● DELETE /api/articles-tags/:articleTagId → Remover etiqueta de artículo. (solo autor)
