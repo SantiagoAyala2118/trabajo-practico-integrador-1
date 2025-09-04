1bc61efdd08479529de5f280e46012cf16e81a59
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-04 15:02:04 -0300

    Rutas y middlewares añadidos correctamente

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,1 +1,60 @@
 import { Router } from "express";
+import { authAdminMiddleware } from "../middlewares/adminMiddleware.js";
+import {
+  createTagValidations,
+  deleteTagValidations,
+  getTagValidations,
+  updateTagValidations,
+} from "../middlewares/validations/tag.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+import { authMiddleware } from "../middlewares/auth.js";
+import {
+  createTag,
+  deleteTag,
+  getAllTags,
+  getTag,
+  updateTag,
+} from "../controllers/tag.controller.js";
+
+const tagRouter = Router();
+
+tagRouter.post(
+  "/api/tags",
+  authAdminMiddleware,
+  createTagValidations,
+  applyValidations,
+  createTag
+);
+
+tagRouter.get("/api/tags", authMiddleware, getAllTags);
+
+tagRouter.get(
+  "/api/tags/:id",
+  authAdminMiddleware,
+  getTagValidations,
+  applyValidations,
+  getTag
+);
+
+tagRouter.put(
+  "/api/tags/:id",
+  authAdminMiddleware,
+  updateTagValidations,
+  applyValidations,
+  updateTag
+);
+
+tagRouter.delete(
+  "/api/tags/:id",
+  authAdminMiddleware,
+  deleteTagValidations,
+  applyValidations,
+  deleteTag
+);
+
+// Tags:
+// ● POST /api/tags → Crear etiqueta (solo admin).
+// ● GET /api/tags → Listar todas las etiquetas. (usuario autenticado)
+// ● GET /api/tags/:id → Obtener etiqueta específica con artículos asociados(solo admin).
+// ● PUT /api/tags/:id → Actualizar etiqueta (solo admin).
+// ● DELETE /api/tags/:id → Eliminar etiqueta (solo admin).
