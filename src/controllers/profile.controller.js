5c2e16ca11929f84d39ce67285e88ac5e8750e7a
Santiago Ayala <santitomayala07@gmail.com>
2025-09-03 23:29:26 -0300

    Funciones para actualizar un usuario y traerlo añadidas

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,9 +1,22 @@
 import { ProfileModel } from "../models/profile.model.js";
+import { UserModel } from "../models/user.model.js";
+import { matchedData } from 'express-validator'
 
 //TRAER UN PERFIL (USUARIO AUTENTICADO)
 export const getProfile = async (req, res) => {
+  const userLogged = req.userLogged;
   try {
-    const profile = await ProfileModel.findOne({ where: {} });
+    const profile = await ProfileModel.findOne({ where: { user_id: userLogged.id } });
+
+    if (!profile) {
+      return res.status(401).json({
+        message: 'You are no authorized'
+      })
+    }
+
+    return res.status(200).json({
+      profile: profile
+    })
   } catch (err) {
     console.error("Server error while getting profile", err);
     return res.status(500).json("Server error while getting profile");
@@ -12,7 +25,29 @@ export const getProfile = async (req, res) => {
 
 //ACTUALIZAR UN USUARIO
 export const updateProfile = async (req, res) => {
+  const userLogged = req.userLogged;
   try {
+    const userAuth = await UserModel.findOne({ where: { id: userLogged.id } });
+
+    if (!userAuth) {
+      return res.status(401).json({
+        message: 'You are not authorized'
+      })
+    }
+
+    const validatedData = matchedData(req, { location: ['body'] });
+
+    if (Object.keys(validatedData) === 0) {
+      return res.status(400).json({
+        message: 'You did not send anything to update'
+      })
+    }
+
+    await ProfileModel.update(validatedData, { where: { user_id: userAuth.id } })
+
+    return res.status(200).json({
+      message: 'Profile updated'
+    })
   } catch (err) {
     console.error("Server error while updating a profile", err);
     return res.status(500).json({
