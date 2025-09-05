3854ba8ebff60a034662757e816a54d232f60373
Santiago Ayala <santitomayala07@gmail.com>
2025-09-04 23:31:42 -0300

    Ruta exportada

--- src/routes/article_tag.routes.js
+++ src/routes/article_tag.routes.js
@@ -28,6 +28,7 @@ articleTagsRouter.delete(
   deleteArticleTag
 );
 
+export default articleTagsRouter;
 // Articles Tags:
 // ● POST /api/articles-tags → Agregar etiqueta a artículo. (solo autor)
 // ● DELETE /api/articles-tags/:articleTagId → Remover etiqueta de artículo. (solo autor)
